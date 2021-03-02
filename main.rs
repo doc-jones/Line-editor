f6a21c5414fe3c6fb14499630477f7006f4eebf9
Doc <doc.jones@tutanota.com>
2021-03-02 08:30:53 +0000

    add KeyCode attrib inside loop

--- main.rs
+++ main.rs
@@ -1,36 +1,50 @@
 use std::io::{stdout, Write};
 
 use crossterm::{
-    event::read,
-    event::Event,
+        event::{KeyEvent, KeyCode, Event}, 
+    event::{read, DisableMouseCapture, EnableMouseCapture},
     execute,
     style::{Color, Print, ResetColor, SetBackgroundColor, SetForegroundColor},
-    ExecutableCommand, Result,
+    terminal, ExecutableCommand, Result,
 };
 
 fn main()-> Result<()> {
+    let mut buffer = String::new();
+
+
     stdout()
         .execute(SetForegroundColor(Color::Blue))?
         .execute(SetBackgroundColor(Color::Red))?
         .execute(Print("Styled text here."))?
         .execute(ResetColor)?
-        .execute(EnableMouseCapture)?;
+        // .execute(EnableMouseCapture)?;
 
     terminal::enable_raw_mode()?;
 
-    match event::read()? {
-        Event::Key(event) => {
-            println!("{:?}", event)
-        }
-        Event::Mouse(event) => {
-            println!("{:?}", event)
-        }
-        Event::Resize(width, height) => {
-            println!("width: {} and height: {}", width, height)
+    loop {
+        match read()? {
+            Event::Key(KeyEvent {code, modifiers}) => {
+                match code {
+                    KeyCode::Char(c) => {
+                        buffer.push(c);
+                    }
+                    KeyCode::Enter => {
+                        break;
+                    }
+                };
+                println!("{:?}", event)
+            }
+            Event::Mouse(event) => {
+                println!("{:?}", event)
+            }
+            Event::Resize(width, height) => {
+                println!("width: {} and height: {}", width, height)
+            }
         }
     }
 
-    stdout().execute(DisableMouseCapture)?;
+
+    // stdout().execute(DisableMouseCapture)?;
 
     terminal::disable_raw_mode()?;
 
