97a401427f82789309bddb32482733d76df5a078
Doc <doc.jones@tutanota.com>
2021-03-02 07:47:09 +0000

    add crossterm attribs and text colors

---
+++ main.rs
@@ -0,0 +1,17 @@
+use std::io::{stdout, Write};
+
+use crossterm::{
+    event, execute,
+    style::{Color, Print, ResetColor, SetBackgroundColor, SetForegroundColor},
+    ExecutableCommand, Result,
+};
+
+fn main()-> Result<()> {
+    stdout()
+        .execute(SetForegroundColor(Color::Blue))?
+        .execute(SetBackgroundColor(Color::Red))?
+        .execute(Print("Styled text here."))?
+        .execute(ResetColor)?;
+
+    Ok(())
+}
